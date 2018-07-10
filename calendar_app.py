7f0ab823ed21ea7a20e4105f57026b661b70b0c0
Morrow, Michael John <spyder14@ufl.edu>
2018-07-10 11:19:38 -0400

    removed old files, finally can access sub-calendars

--- calendar_app.py
+++ calendar_app.py
@@ -4,8 +4,13 @@ from exchangelib.folders import Calendar
 credentials = Credentials('spyder14@ufl.edu', 'Li@m1026!')
 account = Account('spyder14@ufl.edu', credentials=credentials, autodiscover=True)
 
-for item in account.calendar.all().order_by('-datetime_received')[:5]:
-    print(item.subject)
+#for item in account.calendar.all().order_by('-datetime_received')[:5]:
+#    print(item.subject)
 
-#for f in account.folders[Calendar]:
-#    print f
+for f in account.root.walk():
+    if f.name == 'Where_is_Mike':
+        my_folder = f
+        for item in my_folder.all().order_by('-datetime_received')[:5]: print(item.subject)
+        break
+else:
+    raise Exception('Folder not found')
